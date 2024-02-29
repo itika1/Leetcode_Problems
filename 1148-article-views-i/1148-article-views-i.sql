6bcdab319aef962d1430584db70e356319b94fef
itika1 <itikaitzme@gmail.com>
2024-02-29 11:55:02 +0530

    Deleted question other than trees

--- 1148-article-views-i/1148-article-views-i.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-select distinct v.author_id as id
-from Views v
-where v.author_id=v.viewer_id
-order by v.author_id;
--- 627-swap-salary/627-swap-salary.sql
+++
@@ -1,3 +0,0 @@
-# Write your MySQL query statement below
-update salary
-set sex=if(sex='f','m','f');
